ffb4298ef1e30895fc6f44de04557d08c9dc79c3
Prabhat <prabhat.maurya@hitachiconsulting.com>
2018-06-22 15:18:50 +0530

    HPL registration application

---
+++ HPL/Controllers/RegistrationController.cs
@@ -0,0 +1,55 @@
+﻿using HPL.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HPL.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(PlayerDetail obj)
+
+        {
+            //var errors = ModelState.Values.SelectMany(v => v.Errors);
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    HPLEntities db = new HPLEntities();
+
+                    if (db.PlayerDetails.Any(pd => pd.EmployeeId == obj.EmployeeId))
+                    {
+                        return Redirect("/AlreadyRegistered.html");
+                    }
+                    string _FileName = Path.GetFileNameWithoutExtension(obj.UploadedImage.FileName);
+                    string _Extension = Path.GetExtension(obj.UploadedImage.FileName);
+                    _FileName = _FileName + "_" + DateTime.Now.ToString("mmddyyyy") + _Extension;
+                    obj.ImageUrl = "~/UploadedImages/" + _FileName;
+                    _FileName = Path.Combine(Server.MapPath("~/UploadedImages"), _FileName);
+                    obj.UploadedImage.SaveAs(_FileName);
+                                        
+                    db.PlayerDetails.Add(obj);
+                    db.SaveChanges();
+                    ModelState.Clear();
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+            //return View(obj);
+
+            return Redirect("/RegistrationComplete.html");
+        }
+    }
+}
---
+++ HPL/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HPL.MvcApplication" Language="C#" %>
